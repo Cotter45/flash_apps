43fcfaa6ecddaad5a7daf194e045181fc52638a5
Sean <sd.cotter45@gmail.com>
2021-12-06 15:12:39 -0500

    pemdas calculator done

--- app/routes/calculator.tsx
+++ app/routes/calculator.tsx
@@ -10,18 +10,25 @@ export let links: LinksFunction = () => {
 };
 
 
-
 export default function Calculator() {
+    const operators = ['+', '-', 'X', '/', '+/-', 'C', '%'];
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     const [value, setValue] = useState('');
 
     const handleClick = (e: any) => {
-        const operators = ['+', '-', 'X', '/', '+/-', 'C', '%'];
-        const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
         if (numbers.includes(e.target.innerText)) {
-            setValue(value + e.target.innerText);
+            setValue(value + parseInt(e.target.innerText, 10));
+        } else if (e.target.innerText === '+/-') {
+            let vals = value.split(' ');
+            let val: any = vals.pop();
+            if (val) {
+                val = parseInt(val, 10) * -1;
+                vals.push(val);
+                setValue(vals.join(' '));
+            }
         } else {
             setValue(value + ' ' + e.target.innerText + ' ');
         }
@@ -31,7 +38,44 @@ export default function Calculator() {
     }
 
     const calculate = () => {
+        let calc = value.split(' ');
+        let result = 0;
+        let temp = 0;
+        let operator = '';
+
+        let multiples = calc.indexOf('X') > -1 ? calc.splice(calc.indexOf('X') - 1, 3): [];
+        let divisors = calc.indexOf('/') > -1 ? calc.splice(calc.indexOf('/') - 1, 3): [];
+        let mods = calc.indexOf('%') > -1 ? calc.splice(calc.indexOf('%') - 1, 3): [];
         
+        if (multiples.length > 0) {
+            result += parseInt(multiples[0], 10) * parseInt(multiples[2], 10);
+        }
+        if (divisors.length > 0) {
+            result += parseInt(divisors[0], 10) / parseInt(divisors[2], 10);
+        }
+        if (mods.length > 0) {
+            result += parseInt(mods[0], 10) % parseInt(mods[2], 10);
+        }
+
+        for (let i = 0; i < calc.length; i++) {
+            
+            if (operators.includes(calc[i])) {
+                operator = calc[i];
+                continue;
+            } else {
+                temp = parseInt(calc[i], 10);
+            }
+
+            if (operator === '+') {
+                result = result + temp;
+            } else if (operator === '-') {
+                result = result - temp;
+            } else if (operator === '') {
+                result += temp;
+            }
+        }
+
+        setValue(`${result}`);
     }
 
   return (
