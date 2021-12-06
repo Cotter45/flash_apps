71798a0dceeed6c59d3020a452a06a1d64fe953a
Sean <sd.cotter45@gmail.com>
2021-12-06 15:21:23 -0500

    fixed some spacing and syntax

--- app/routes/calculator.tsx
+++ app/routes/calculator.tsx
@@ -11,13 +11,13 @@ export let links: LinksFunction = () => {
 
 
 export default function Calculator() {
+    
     const operators = ['+', '-', 'X', '/', '+/-', 'C', '%'];
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     const [value, setValue] = useState('');
 
     const handleClick = (e: any) => {
-
 
         if (numbers.includes(e.target.innerText)) {
             setValue(value + parseInt(e.target.innerText, 10));
